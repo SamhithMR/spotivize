90c0d55b85aedf4602372ffd52d2d85f701996e3
samhithMR <samhith.mr@gmail.com>
2023-04-26 21:21:55 +0530

    implemented a d3js component to visualize average time spent by user w.r.t hours

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import Timespent from './components/Timespent'
+import TimeSpendPerHour from './components/TimeSpendPerHour'
 import Login from './components/login'
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
-import {getToken} from './store/credentials'
+import {getToken, getTrack} from './store/credentials'
+import { fetchDataFromApi } from './utils/api';
 
 function App() {
   const dispatch = useDispatch()
   const token = useSelector((state) => state.Credentials.token)
 
   const hash = window.location.hash
+
   useEffect(()=>{
-    dispatch(getToken(hash.substring(1).split("&")[0].split("=")[1]))
+    const token_value = hash.substring(1).split("&")[0].split("=")[1];
+    dispatch(getToken(token_value))
+
+    console.log(token_value);
+    const today = new Date();
+    const lastWeekTimestamp = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7, 0, 0, 0).getTime();
+    fetchDataFromApi(`/me/player/recently-played?after=${lastWeekTimestamp}&limit=50`,token_value)
+    .then((res)=>{
+      // console.log(res);
+      dispatch(getTrack({data:res,loading:false}))
+    })
+    .catch((err)=>{
+      console.log("from app.js" + err);
+    })
   },[hash])
 
   return (
     <div>
       {/* <Recent /> */}
-      {token ? <Timespent /> : <Login />}
+      {token ? <TimeSpendPerHour /> : <Login />}
     </div>
   )
 }
--- src/store/credentials.js
+++ src/store/credentials.js
@@ -3,15 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 export const Credentials = createSlice({
     name: "Credentials",
     initialState: {
-        token: ""
+        token: "",
+        recentTracks:{data:{},loading:true}
     },
     reducers: {
         getToken: (state, action) => {
             state.token = action.payload;
+        },
+        getTrack: (state, action)=>{
+            state.recentTracks = action.payload
         }
     },
 });
 
-export const { getToken } = Credentials.actions;
+export const { getToken, getTrack } = Credentials.actions;
 
 export default Credentials.reducer;
